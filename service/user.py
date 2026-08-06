98ccad4468ea64d6f49d07df12058f613367ab51
Hifza-155 <hafsausman1512@gmail.com>
2026-08-06 20:10:13 +0500

    Resolved module not found error as I imported schemas.user instead schemas.userDTO

--- service/user.py
+++ service/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from schemas.user import UserCreate
+from schemas.userDTO import UserCreate
 from repository import user as user_repo
 
 def create_user(db: Session, user_data: UserCreate, auth_provider_uid: str):
