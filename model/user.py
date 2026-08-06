616a0a01eb6a73db1cfd80567608aacf67d78d5c
Hifza-155 <hafsausman1512@gmail.com>
2026-08-06 12:25:56 +0500

    Imports Added for CITEXT and checkConstraint

--- model/user.py
+++ model/user.py
@@ -1,12 +1,21 @@
-from sqlalchemy import Column, Integer, String, DateTime
+# 1. We added CheckConstraint and Text here
+from sqlalchemy import Column, String, DateTime, CheckConstraint, Text
 from sqlalchemy.sql import func
 from database import Base
 
+from sqlalchemy.dialects.postgresql import CITEXT, UUID
+import uuid
+
 class User(Base):
     __tablename__ = "user_account"
-    id = Column(Integer, primary_key=True, index=True)
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    
     email = Column(CITEXT, unique=True, nullable=False)
-    full_name = Column(String, nullable=True)
+    
+    full_name = Column(Text, nullable=False)
+    
+    auth_provider_uid = Column(Text, unique=True, nullable=True)
         
     # Timestamps
     last_login_at = Column(DateTime(timezone=True), nullable=True)
