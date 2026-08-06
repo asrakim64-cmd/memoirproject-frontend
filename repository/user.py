7c1395e345c6441c257180b11646365408c696db
Hifza-155 <hafsausman1512@gmail.com>
2026-08-06 20:19:18 +0500

    Entity name is User but accidently wrote UserAccount that was causing error

--- repository/user.py
+++ repository/user.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
-from model.user import UserAccount
+from model.user import User
 
 def create_user_in_db(db: Session, email: str, full_name: str, auth_provider_uid: str):
-    db_user = UserAccount(
+    db_user = User(
         email=email,
         full_name=full_name,
         auth_provider_uid=auth_provider_uid 
