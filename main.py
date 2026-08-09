87924a171efd6662d5ec1eed7cd5ca9df3a4c980
Hifza-155 <hafsausman1512@gmail.com>
2026-08-09 11:15:30 +0500

    Testing code removed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from routers import user 
-from dependencies import verify_supabase_token
 
 app = FastAPI(title="Memoir App API") 
 
@@ -9,10 +8,3 @@ app.include_router(user.router)
 @app.get("/")
 def read_root():
     return {"message": "API is successfully running!"}
-
-@app.post("/api/subscribe")
-def process_subscription(data: dict, user: dict = Depends(verify_supabase_token)):
-    user_id = user.get("sub")
-    plan = data.get("plan")
-    
-    return {"status": "success", "message": f"Subscription active for user {user_id}"}
