acfa1752dd1280f28bcb5a845878907800b3b2ee
Hifza-155 <hafsausman1512@gmail.com>
2026-08-09 13:48:14 +0500

    At first I were implementing authentication through python backend because we dont had frontend to test it .As going to connect frontend with supabase Auth useless code is removed

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from routers import user
 
 app = FastAPI(title="Memoir App API") 
 
-app.include_router(user.router)
-
 @app.get("/")
 def read_root():
     return {"message": "API is successfully running!"}
