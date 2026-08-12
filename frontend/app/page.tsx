2fde272855bea20a2609d10e77c1601d81c80770
Hifza-155 <hafsausman1512@gmail.com>
2026-08-12 23:46:57 +0500

    LandingPageScroll import added an is called in main

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,11 @@
 // The main landing page 
 import Hero from "../components/landingPage/Hero";
-
+import LandingPageScroll from "../components/landingPage/LandingPageScroll";
 export default function Home() {
   return (
     <main>
       <Hero />
+      <LandingPageScroll />
     </main>
   );
 }
