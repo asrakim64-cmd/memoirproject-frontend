586704720cad761b500721ac23e2c9fd0b22d7e3
Hifza-155 <hafsausman1512@gmail.com>
2026-08-13 01:17:07 +0500

    No change

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,7 @@
 // The main landing page 
 import Hero from "../components/landingPage/Hero";
 import LandingPageScroll from "../components/landingPage/LandingPageScroll";
+
 export default function Home() {
   return (
     <main>
