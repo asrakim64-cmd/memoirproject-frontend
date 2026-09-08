726a3f317bef73871b8a77b85bed29fc6e92adef
asrakim64-cmd <asrakim64@gmail.com>
2026-09-08 18:51:35 +0500

    add memoir PDF browser viewer

--- src/features/MemoirPDF/MemoirPDFViewer.tsx
+++ src/features/MemoirPDF/MemoirPDFViewer.tsx
@@ -1,1 +1,30 @@
 // PDF viewer component
+"use client";
+
+import dynamic from "next/dynamic";
+
+const PDFViewer = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+  }
+);
+
+const MemoirPDFDocument = dynamic(
+  () => import("./MemoirPDFDocument"),
+  {
+    ssr: false,
+  }
+);
+
+export default function MemoirPDFViewer() {
+  return (
+    <PDFViewer
+      width="100%"
+      height="100%"
+      showToolbar
+    >
+      <MemoirPDFDocument />
+    </PDFViewer>
+  );
+}
