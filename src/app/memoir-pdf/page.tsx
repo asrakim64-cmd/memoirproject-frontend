4b16010454d6c1d5f4ddeb6a7b5ad5b220b8bb17
asrakim64-cmd <asrakim64@gmail.com>
2026-09-08 18:52:39 +0500

    connect memoir PDF page to viewer

--- src/app/memoir-pdf/page.tsx
+++ src/app/memoir-pdf/page.tsx
@@ -1,1 +1,14 @@
 // PDF page entry point
+"use client";
+
+import MemoirPDFViewer from "../../features/MemoirPDF/MemoirPDFViewer";
+
+export default function MemoirPDFPage() {
+  return (
+    <main className="min-h-screen bg-[#EDE6DE] p-4 sm:p-6">
+      <div className="mx-auto h-[calc(100vh-2rem)] max-w-6xl overflow-hidden rounded-xl bg-white shadow-lg sm:h-[calc(100vh-3rem)]">
+        <MemoirPDFViewer />
+      </div>
+    </main>
+  );
+}
